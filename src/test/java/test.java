2517ce5a983bb1123c957057bf282faa9790415b
aidamarku0617 <aidamarku0617@gmail.com>
2024-02-14 20:01:11 -0500

    second

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,5 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("i am batch18 student");
+        System.out.println("new commit by new user");
+
     }
 }
